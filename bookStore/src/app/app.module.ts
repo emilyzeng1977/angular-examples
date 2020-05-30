25944faec4c444b0d3f0a48d2e26e7e50cb4eab6
tniu <tniu@infomedia.com.au>
2020-05-30 20:48:17 +1000

    Init

---
+++ bookStore/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { FormsModule } from "@angular/forms";
+import { BookDetailComponent } from './book-detail/book-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksComponent,
+    BookDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bookStore/src/app/books/books.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from "../book";
+import { BOOKS } from "../mock-books";
+import {BookService} from "../book.service";
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  books: Book[];
+  selectedBook: Book;
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    this.books = this.bookService.getBooks();
+  }
+
+  onSelect(book: Book): void {
+    this.selectedBook = book;
+  }
+}
