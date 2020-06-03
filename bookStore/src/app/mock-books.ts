34d4e88df2a4f3878b326dff2ac994018751a68e
tniu <tniu@infomedia.com.au>
2020-06-03 22:17:55 +1000

    Init

---
+++ bookStore/src/app/mock-books.ts
@@ -0,0 +1,7 @@
+import { Book } from "./book";
+
+export const BOOKS: Book[] = [
+  { id: 11, name: 'Harry Potter', price: 50 },
+  { id: 12, name: 'Narco', price: 12 },
+  { id: 13, name: 'Bombasto', price: 30 },
+];
